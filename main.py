a0da70cbb3d5df6b056c11f8002dc9fea195fafd
Vasi_lena <105110262+Copynook@users.noreply.github.com>
2022-10-16 19:03:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import turtle
+import time
+
+WIDTH, HEIGHT = 500, 500
+
+def get_number_of_racers():
+    racers = 0
+    while True:
+        racers = input('Enter the number of racers (2 - 10): ')
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print('Input is not numberic... Try again!')
+            continue
+
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print('Number not in range 2-10. Please, try again.')
+
+def init_turtle():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title('Turtle Racing!') 
+
+racers = get_number_of_racers()
+init_turtle()
+
+racer = turtle.Turtle()
+racer.forward(100)
+time.sleep(5)
